073bbcf5d3ca8800d12b40a1eb1ba167edca883a
Keith Leung <kykleung@gmail.com>
2013-10-18 12:27:59 -0300

    Updated license

--- scripts/appendLicense.sh
+++ scripts/appendLicense.sh
@@ -3,30 +3,40 @@
 
 for f in ../README; do
   if grep -q 'Software License Agreement (New BSD License)' "$f";then 
-    echo "No need to copy the License Header to $f"
-  else
-    cat ../License $f > $f.new
-    mv $f.new $f
-    echo "License Header copied to $f"
-  fi 
+    sed -i '1,/^$/ d' $f 
+    echo "Removing License Header in $f"
+  fi
+  cat ../License $f > $f.new
+  mv $f.new $f
+  echo "New license Header copied to $f" 
 done  
 
 for f in ../include/*.hpp; do
   if grep -q 'Software License Agreement (New BSD License)' "$f";then 
-    echo "No need to copy the License Header to $f"
-  else
-    cat ../License $f > $f.new
-    mv $f.new $f
-    echo "License Header copied to $f"
-  fi 
+    sed -i '1,/^$/ d' $f 
+    echo "Removing License Header in $f"
+  fi
+  cat ../License $f > $f.new
+  mv $f.new $f
+  echo "New License Header copied to $f" 
 done  
 
 for f in ../src/*.cpp; do
   if grep -q 'Software License Agreement (New BSD License)' "$f";then 
-    echo "No need to copy the License Header to $f"
-  else
-    cat ../License $f > $f.new
-    mv $f.new $f
-    echo "License Header copied to $f"
-  fi 
+    sed -i '1,/^$/ d' $f 
+    echo "Removing License Header in $f"
+  fi
+  cat ../License $f > $f.new
+  mv $f.new $f
+  echo "New License Header copied to $f" 
+done  
+
+for f in ../cfg/*; do
+  if grep -q 'Software License Agreement (New BSD License)' "$f";then 
+    sed -i '1,/^$/ d' $f 
+    echo "Removing License Header in $f"
+  fi
+  cat ../License $f > $f.new
+  mv $f.new $f
+  echo "New License Header copied to $f" 
 done  
