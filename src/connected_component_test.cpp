63feb4b188f27fc19fc8c9f775c4ca445450f803
kykleung <kykleung@gmail.com>
2014-01-09 15:07:55 -0300

    Added test for matrix partitioning

--- src/connected_component_test.cpp
+++ src/connected_component_test.cpp
@@ -3,8 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/connected_components.hpp>
+#include "LinearAssignment.hpp"
 
 #include <boost/timer/timer.hpp>
 #include <boost/format.hpp>
@@ -46,44 +45,33 @@ int main(int argc, char *argv[])
 
   boost::timer::cpu_timer timer;
 
-  boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> G;
-  for(int i = 0; i < C_size; i++){
-    for(int j = 0; j < C_size; j++){
-      if (C[i][j] != 0 ){
-	boost::add_edge(i, j+C_size, G);
-      }
-    }
-  }
-  std::vector<int> cc_results(boost::num_vertices(G));
-  int nConnectedComponents = boost::connected_components(G, &cc_results[0]);
-
-  std::vector<unsigned int> components_m [nConnectedComponents];
-  for(int i = 0; i <  C_size; i++){
-    components_m[cc_results[i]].push_back(i);
-  }
-  std::vector<unsigned int> components_z [nConnectedComponents];
-  for(int i = C_size; i < C_size*2; i++){
-    components_z[cc_results[i]].push_back(i - C_size);
-  }
-
+  CostMatrix likelihoodMat(C, C_size);
+  int nP = likelihoodMat.partition();
+  
   boost::timer::cpu_times t = timer.elapsed();
   std::cout << boost::format("Elapsed time: %1% [ns]\n") % t.wall;
 
-  for(int i = 0; i < nConnectedComponents; i++){
-    printf("Component %d: ", i);
-    for(int j = 0; j < components_m[i].size(); j++){
-	printf("m%d ", components_m[i][j]);
+  for(int n = 0; n < nP; n++){
+    
+    size_t nCols, nRows;
+    likelihoodMat.getPartitionSize(n, nCols, nRows);
+    double** Cp = new double* [nCols + nRows];
+    for(int i = 0; i < nCols + nRows; i++){
+      Cp[i] = new double[nCols + nRows];
     }
-    for(int j = 0; j < components_z[i].size(); j++){
-	printf("z%d ", components_z[i][j]);
-    }
-    printf("\n");
-    for(int m = 0; m < components_m[i].size(); m++){
-      for(int n = 0; n < components_z[i].size(); n++){
-	printf("%f  ", C[ components_m[i][m] ][ components_z[i][n] ]);
-      }  
+    likelihoodMat.getPartition(n, Cp);
+    for(int i = 0; i < nRows; i++){
+      for(int j = 0; j < nCols; j++){
+	printf("%f  ", Cp[i][j]);
+      }
       printf("\n");
     }
+    
+    for(int i = 0; i < nCols + nRows; i++){
+      delete[] Cp[i];
+    }
+    delete[] Cp;
+
   }
 
   for(int i = 0; i < C_size; i++){
