0347d462aeea7e2abd7e2cc075c008231f4b8331
kykleung <kykleung@gmail.com>
2014-12-16 15:20:08 -0300

    Made some updates to OSPA and Hungarian method class

--- include/OSPA.hpp
+++ include/OSPA.hpp
@@ -32,145 +32,160 @@
 #define OSPA_HPP
 
 #include <algorithm>
+#include <boost/multi_array.hpp>
+#include <boost/shared_array.hpp>
 #include <cstddef>
 #include <math.h>
-#include <vector>
 #include <stdio.h>
+#include <vector>
+
 #include "HungarianMethod.hpp"
 
 namespace rfs{
 
-/**
- * \class OSPA
- * Optimal Sub-pattern Assignment metric
- * \f[ e_{\textrm{OSPA}} = \Bigg(\frac{1}{\left|\mathcal{M}_g\right|} \Bigg[ \min_{j \in \left\{ 1 \ldots \left|\mathcal{M}_g\right|\right\}} \sum_{i=1}^{\left|\mathcal{M}_k\right|} \min \left(d_{i,j},c\right)^p + c^p \Big| \left|\mathcal{M}_g\right| - \left|\mathcal{M}_k\right| \Big| \Bigg] \Bigg)^{\frac{1}{p}} \f]
- * \brief A class for computing the optimal sub-pattern assignment metric given 2 sets
- * \tparam SetElementType The class representing an element of the sets to be compared. The operator - must be defined in this class to return the distance between elements
- * \author Keith Leung
- */
-template<class SetElementType> 
-class OSPA{
-
-public:
-
-  /** Constructor 
-   * \param set1 a std::vector containing the elements in set 1
-   * \param set2 a std::vector containing the elements in set 2
-   * \param cutoff value c
-   * \param order value p
+  /**
+   * \class OSPA
+   * Optimal Sub-pattern Assignment metric
+   * \f[ e_{\textrm{OSPA}} = \Bigg(\frac{1}{\left|\mathcal{M}_g\right|} \Bigg[ \min_{j \in \left\{ 1 \ldots \left|\mathcal{M}_g\right|\right\}} \sum_{i=1}^{\left|\mathcal{M}_k\right|} \min \left(d_{i,j},c\right)^p + c^p \Big| \left|\mathcal{M}_g\right| - \left|\mathcal{M}_k\right| \Big| \Bigg] \Bigg)^{\frac{1}{p}} \f]
+   * \brief A class for computing the optimal sub-pattern assignment metric given 2 sets
+   * \tparam SetElementType The class representing an element of the sets to be compared. The operator - must be defined in this class to return the distance between elements
+   * \author Keith Leung
    */
-  OSPA(std::vector<SetElementType> &set1,
-       std::vector<SetElementType> &set2,
-       double cutoff,
-       double order);
+  template<class SetElementType> 
+  class OSPA{
 
-  /** Destructor */
-  ~OSPA();
+  public:
 
-  /** Calculate the OSPA error
-   * \param[out] distance error component (before raising to power p_ and averaging over n_) 
-   * \param[out] cardinality error component (before raising to power p_ and averaging over n_)
-   * \param[in] report True to generate report to stdout
-   * \return OSPA error
-   */
-  double calcError(double *e_dist = NULL, double *e_card = NULL, bool report = false);
+    /** Cost matrix */
+    typedef boost::multi_array<double, 2> CostMat;
 
-private:
+    /** Cost matrix index */
+    typedef CostMat::index Idx;
 
-  unsigned int n1_; /**< size of set 1 */
-  unsigned int n2_; /**< size of set 2 */
-  unsigned int n_; /**< size of cost matrix */
-  double** C_; /**< cost matrix */
-  double c_; /**< cutoff threshold */
-  double p_; /**< order of metric */
+    /** Solution array */
+    typedef boost::shared_array<int> SolnArr;
 
-};
+    /** Constructor 
+     * \param set1 a std::vector containing the elements in set 1
+     * \param set2 a std::vector containing the elements in set 2
+     * \param cutoff value c
+     * \param order value p
+     */
+    OSPA(std::vector<SetElementType> &set1,
+	 std::vector<SetElementType> &set2,
+	 double cutoff,
+	 double order);
 
-// Implementation
+    /** Destructor */
+    ~OSPA();
 
-template<class SetElementType>
-OSPA<SetElementType>::OSPA(std::vector<SetElementType> &set1,
-			   std::vector<SetElementType> &set2,
-			   double cutoff,
-			   double order) : c_(cutoff),
-					   p_(order)
+    /** Calculate the OSPA error
+     * \param[out] distance error component (before raising to power p_ and averaging over n_) 
+     * \param[out] cardinality error component (before raising to power p_ and averaging over n_)
+     * \param[in] report True to generate report to stdout
+     * \return OSPA error
+     */
+    double calcError(double *e_dist = NULL, double *e_card = NULL, bool report = false);
+
+    /** Report the optimal assignment using std::cout */
+    void reportSoln();
+
+    /** Get the optimal assignment */
+    SolnArr getOptAssignment();
+
+  private:
+    
+    double c_; /**< cutoff threshold */
+    double p_; /**< order of metric */
+    CostMat C_; /**< Cost matrix */
+    unsigned int n1_; /**< size of set 1 */
+    unsigned int n2_; /**< size of set 2 */
+    unsigned int n_; /**< size of cost matrix */
+    SolnArr soln_; /**< Solution array */
+    double cost_; /**< OSPA error */
+
+  };
+
+  // Implementation
+
+  template<class SetElementType>
+  OSPA<SetElementType>::OSPA(std::vector<SetElementType> &set1,
+			     std::vector<SetElementType> &set2,
+			     double cutoff,
+			     double order) : c_(cutoff),
+					     p_(order)
 							      
-{
+  {
 
-  // Allocate memory for cost matrix
-  n1_ = set1.size();
-  n2_ = set2.size();
-  n_ = std::max(n1_, n2_);
-  C_ = new double*[n_];
-  for( int i = 0; i < n_; i++){
-    C_[i] = new double[n_];
-  }
+    // Allocate memory for cost matrix
+    n1_ = set1.size();
+    n2_ = set2.size();
+    n_ = std::max(n1_, n2_);
 
-  // Construct cost matrix 
-  for( int i = 0; i < n1_; i++){
-    for( int j = 0; j < n2_; j++){      
-      C_[i][j] = fabs(set1[i] - set2[j]);
-      if(C_[i][j] > c_)
-	C_[i][j] = c_;
-    }
-  }
-  if( n1_ < n_ ){ // n1 < n2 
-    for( int i = n1_; i < n_; i++){
-      for( int j = 0; j < n_; j++){
-	C_[i][j] = c_;
+    // Construct cost matrix
+    C_ = CostMat(boost::extents[n_][n_]);
+    for( Idx i = 0; i < n1_; i++ ){
+      for( Idx j = 0; j < n2_; j++){ 
+	C_[i][j] = fabs(set1[i] - set2[j]);
+	if(C_[i][j] > c_)
+	  C_[i][j] = c_;
       }
     }
-  }else if( n2_ < n_ ){ // n2 < n1
-    for( int i = 0; i < n_; i++){
-      for( int j = n2_; j < n_; j++){
-	C_[i][j] = c_;
+    if( n1_ < n_ ){ // n1 < n2 
+      for( Idx i = n1_; i < n_; i++){
+	for( Idx j = 0; j < n_; j++){
+	  C_[i][j] = c_;
+	}
+      }
+    }else if( n2_ < n_ ){ // n2 < n1
+      for( Idx i = 0; i < n_; i++){
+	for( Idx j = n2_; j < n_; j++){
+	  C_[i][j] = c_;
+	}
       }
     }
+
+    // Use the Hungarian method to find optimal assignment
+    rfs::HungarianMethod hm;
+    soln_.reset( new int[n_] );
+    double cost; // This cost is not the ospa cost because order p is not accounted for
+    hm.run<CostMat>(C_, n_, soln_.get(), &cost, false);
+
   }
 
-}
+  template<class SetElementType>
+  OSPA<SetElementType>::~OSPA(){}
 
-template<class SetElementType>
-OSPA<SetElementType>::~OSPA(){
-
-  // Deallocate memory for cost matrix
-  for( int i = 0; i < n_; i++){
-    delete[] C_[i];
-  }
-  delete[] C_;
-
-}
-
-
-template<class SetElementType>
-double OSPA<SetElementType>::calcError(double *e_dist, double *e_card, bool report){
+  template<class SetElementType>
+  double OSPA<SetElementType>::calcError(double *e_dist, double *e_card, bool report){
+ 
+    cost_ = 0;
+    if(e_dist != NULL)
+      *e_dist = 0;
+    if(e_card != NULL)
+      *e_card = 0;
   
-  rfs::HungarianMethod hm;
-  int soln[n_];
-  double cost;
-  hm.run(C_, n_, soln, &cost, false);
+    for(Idx i = 0; i < n_; i++){
+      unsigned int j = soln_[i];
+      if( C_[i][j] == c_ && e_card != NULL){
+	*e_card += C_[i][j];
+      }else if(e_dist != NULL){
+	*e_dist += C_[i][j];
+      }
+      cost_ += pow(C_[i][j], p_);
+    }
+    cost_ = pow(cost_ / n_, 1.0 / p_);
 
-  if(report){
+    return cost_;
+  }
+  
+  template<class SetElementType>
+  void OSPA<SetElementType>::reportSoln(){
+
     printf("Assignment Results:\n\n");
-  }
 
-  if(e_dist != NULL)
-    *e_dist = 0;
-
-  if(e_card != NULL)
-    *e_card = 0;
-  
-  cost = 0; // OSPA cost is different than Hungarian method cost due to cutoff threshold
-  for(int i = 0; i < n_; i++){
-    unsigned int j = soln[i];
-    if( C_[i][j] == c_ && e_card != NULL){
-      *e_card += C_[i][j];
-    }else if(e_dist != NULL){
-      *e_dist += C_[i][j];
-    }
-    cost += pow(C_[i][j], p_);
-
-    if(report){
+    for(Idx i = 0; i < n_; i++){
+      Idx j = soln_[i];
       if(i >= n1_){
 	printf("n/a -- %03d [%f]\n", j, C_[i][j]);
       }else if(j >= n2_){
@@ -179,17 +194,16 @@ double OSPA<SetElementType>::calcError(double *e_dist, double *e_card, bool repo
 	printf("%03d -- %03d [%f]\n", i, j, C_[i][j]);
       }
     }
+    printf("\nOSPA Error: %f\n", cost_);
     
   }
-  cost = pow(cost / n_, 1.0 / p_);
 
-  if(report){
-    printf("\nOSPA Error: %f\n", cost);
+  template<class SetElementType>
+  typename OSPA<SetElementType>::SolnArr OSPA<SetElementType>::getOptAssignment(){
+
+    return soln_; 
+
   }
-    
-  return cost;
-}
-
 
 } // namespace rfs
 
